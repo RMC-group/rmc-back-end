0c382b3d47bfabc7cc238c9a5c8838ec9542d2cd
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-01-29 01:04:26 +0530

    user api add

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthControllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//user routes
+Route::post('/register', [AuthControllers::class, 'registerUser'])->name('registerUser');
+Route::get('/logout', [AuthControllers::class, 'logout'])->name('logout');
+Route::get('/getAllUser', [AuthControllers::class, 'getAllUser']);
+Route::get('/userDetails', [AuthControllers::class, 'userDetails'])->name('userDetails');
+Route::put('/userUpdate/{id}', [AuthControllers::class, 'userUpdate'])->name('userUpdate');
+
+Route::post('/login', [AuthControllers::class, 'login'])->name('login');
