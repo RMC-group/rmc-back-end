2dcd4e61fad05444bd1c1696f97d2ea472764a3f
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-02-10 16:28:26 +0530

    add content

--- app/Http/Controllers/CustomerControllers.php
+++ app/Http/Controllers/CustomerControllers.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerControllers extends Controller
 {
